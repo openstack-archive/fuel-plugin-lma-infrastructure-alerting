112df8d67e7affb87c9cd4f46cfdf502c69d7f0a
Swann Croiset <scroiset@mirantis.com>
2015-06-30 11:48:42 +0200

    Configure Nagios to manage service status alerts
    * configure Nagios server and Nagios CGI
    * create a Nagios "virtual" hostname pointing to the management VIP
    * create service passive checks carresponding to OpenStack service
      statutes sent by LMA collector
    * create one Contact with an email for alert notifications
    
    implements blueprint lma-infra-alerting-plugin
    
    Change-Id: I6faa3a0274b5b7f345a240d9d0ac7925de8a9919

---
+++ deployment_scripts/puppet/manifests/nagios.pp
@@ -0,0 +1,49 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+$management_vip = hiera('management_vip')
+$env_id = hiera('deployment_id', '?')
+
+$plugin = hiera('lma_infrastructure_alerting')
+$password = $plugin['nagios_http_password']
+$email = $plugin['email']
+$notify_warning = $plugin['notify_warning']
+$notify_critical = $plugin['notify_critical']
+$notify_unknown = $plugin['notify_unknown']
+$notify_recovery = $plugin['notify_recovery']
+
+notice($notify_warning)
+$ceilometer = hiera('ceilometer')
+$services = {}
+if $ceilometer['enabled'] {
+  $services['openstack.ceilometer.status'] = true
+}
+
+if $plugin['node_name'] == hiera('user_node_name') {
+  class { 'lma_infra_alerting::nagios':
+    openstack_deployment_name => $env_id,
+    openstack_management_vip => $management_vip,
+    # additional services
+    services => keys($services),
+    # UI password
+    password => $password,
+    # notifications options
+    contact_email => $email,
+    notify_warning => $notify_warning,
+    notify_critical => $notify_critical,
+    notify_recovery => $notify_recovery,
+    notify_unknown => $notify_unknown,
+  }
+}
