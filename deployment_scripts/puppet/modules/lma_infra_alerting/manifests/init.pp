91578e758a641758b12180f8517f270e641c01b4
Swann Croiset <scroiset@mirantis.com>
2015-06-30 11:48:42 +0200

    Configure Nagios to manage service status alerts
    * configure Nagios server and Nagios CGI
    * create a Nagios "virtual" hostname pointing to the management VIP
    * create service passive checks corresponding to OpenStack service
      statutes sent by LMA collector
    * create one Contact with an email for alert notifications
    
    Note: we ship our own Nagios manifest due to the fact that all manifests
    around the place are either not compatible for masterless Puppet
    environment or not well tested for both Ubuntu and CentOS. Furthermore,
    this plugin probably doesn't require all Nagios configuration features.
    
    implements blueprint lma-infra-alerting-plugin
    
    Change-Id: I6faa3a0274b5b7f345a240d9d0ac7925de8a9919

---
+++ deployment_scripts/puppet/modules/lma_infra_alerting/manifests/init.pp
@@ -0,0 +1,62 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# Configure Nagios server and Nagios CGI
+# Add services status monitoring and contact for notifications
+#
+class lma_infra_alerting (
+  $openstack_management_vip = undef,
+  $openstack_deployment_name = '',
+  $user = $lma_infra_alerting::params::nagios_http_user,
+  $password = $lma_infra_alerting::params::nagios_http_password,
+  $additional_services = [],
+  $contact_email = $lma_infra_alerting::params::nagios_contact_email,
+  $notify_warning = true,
+  $notify_critical = true,
+  $notify_recovery = true,
+  $notify_unknown = true,
+) inherits lma_infra_alerting::params {
+
+  validate_array($additional_services)
+
+  $nagios_openstack_vhostname = $lma_infra_alerting::params::nagios_openstack_hostname_prefix
+  $vhostname = "${nagios_openstack_vhostname}-env${$openstack_deployment_name}"
+
+  $core_openstack_services = $lma_infra_alerting::params::openstack_core_services
+  $all_openstack_services = union($core_openstack_services, $additional_services)
+
+  # Install and configure nagios server
+  class { 'lma_infra_alerting::nagios':
+    http_user => $user,
+    http_password => $password,
+  }
+
+  # Configure services
+  class { 'lma_infra_alerting::nagios::service_status':
+    ip => $openstack_management_vip,
+    hostname => $vhostname,
+    services => $all_openstack_services,
+    require => Class['lma_infra_alerting::nagios'],
+  }
+
+  # Configure contacts
+  class { 'lma_infra_alerting::nagios::contact':
+    email => $contact_email,
+    notify_warning => $notify_warning,
+    notify_critical => $notify_critical,
+    notify_recovery => $notify_recovery,
+    notify_unknown => $notify_unknown,
+    require => Class['lma_infra_alerting::nagios::service_status'],
+  }
+}
