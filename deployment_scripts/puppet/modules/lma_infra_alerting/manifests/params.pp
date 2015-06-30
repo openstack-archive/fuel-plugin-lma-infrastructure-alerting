49efecbc1afce0d2cb584228a373ffc4e234372b
Swann Croiset <scroiset@mirantis.com>
2015-06-30 11:48:42 +0200

    Configure Nagios to manage service status alerts
    * configure Nagios server and Nagios CGI
    * create a Nagios "virtual" hostname pointing to the management VIP
    * create service passive checks corresponding to OpenStack service
      statutes sent by LMA collector
    * create one Contact with an email for alert notifications
    
    Note: we ship our own nagios manifest due to the fact that all manifests
    around the place are either not compatible for masterless puppet
    environment or not well tested for both ubuntu and centOS. Furthermore,
    this plugin probably doesn't require all nagios configuration features.
    
    implements blueprint lma-infra-alerting-plugin
    
    Change-Id: I6faa3a0274b5b7f345a240d9d0ac7925de8a9919

---
+++ deployment_scripts/puppet/modules/lma_infra_alerting/manifests/params.pp
@@ -0,0 +1,51 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+class lma_infra_alerting::params {
+  $nagios_http_user = 'nagiosadmin'
+  $nagios_http_password = ''
+
+  $nagios_config_filename_prefix = 'lma_'
+
+  # Override Nagios server configuration
+  $nagios_command_check_interval = '10s'
+
+  # Following hostname must match with lma_collector::params::nagios_hostname_service_status
+  $nagios_openstack_dummy_hostname = 'openstack-services'
+
+  $nagios_contactgroup = 'openstack'
+  $nagios_contact_email = 'root@localhost'
+  # The 'time unit' for all check_interval and check_retry
+  $nagios_interval_length = '60'
+  # Check and retry intervals are the number of "time units" to wait before
+  #scheduling a (re)check.
+  $nagios_check_interval_service_status = 1
+  # Force an active check (to force UNKNOWN state) if LMA Collector doesn't
+  # push status since the last minute.
+  $nagios_freshness_threshold_service_status = 60
+
+  $nagios_generic_host_template = 'generic-host'
+  $nagios_generic_service_template = 'generic-service'
+
+  # Following service names must be coherent with lma_collector nagios output
+  # plugin names.
+  $openstack_core_services = [
+      'openstack.keystone.status',
+      'openstack.nova.status',
+      'openstack.glance.status',
+      'openstack.cinder.status',
+      'openstack.neutron.status',
+      'openstack.heat.status',
+  ]
+}
---
+++ deployment_scripts/puppet/modules/nagios/manifests/host.pp
@@ -0,0 +1,76 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# == Resource: nagios::host
+#
+# Manage a Nagios host object
+#
+# == Parameters
+# path: the directory conf.d of nagios
+# prefix: an optional prefix of the filename(s)
+# onefile: all objects are defined in one file if true else one file per service and cmd.
+# properties: properties of the nagios_host resource.
+
+define nagios::host (
+  $path = $nagios::params::config_dir,
+  $prefix = '',
+  $onefile = true,
+  $properties = {},
+  $defaults = {},
+  $ensure = present,
+){
+
+  validate_hash($properties, $defaults)
+  $opts = {}
+
+  if is_array($properties['contact_groups']){
+    $opts['contact_groups'] = join($properties['contact_groups'], ',')
+  }else{
+    $opts['contact_groups'] = $properties['contact_groups']
+  }
+
+  if $onefile {
+    $target = "${path}/${prefix}hosts.cfg"
+  }else{
+    $target = "${path}/${prefix}host_${name}.cfg"
+  }
+  $opts['target'] = $target
+  $opts['ensure'] = $ensure
+  $opts['notify'] = Class['nagios::server_service']
+
+  if $properties['host_name'] == undef {
+    $opts['host_name'] = $name
+    $host_name = $name
+  }else{
+    $host_name = $properties['host_name']
+  }
+  if $properties['display_name'] == undef {
+    $opts['display_name'] = $name
+  }
+
+  $params = {
+    "${host_name}" => merge($properties, $opts),
+  }
+
+  create_resources(nagios_host, $params, $defaults)
+
+  if ! defined(File[$target]){
+    file { $target:
+      ensure => $properties['ensure'],
+      mode => '0644',
+      require => Nagios_Host[$host_name],
+      notify => Class['nagios::server_service'],
+    }
+  }
+}
