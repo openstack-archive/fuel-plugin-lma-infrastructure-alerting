7670122b5c89ada769134637179ad70d804b8cbb
Swann Croiset <scroiset@mirantis.com>
2015-07-09 23:19:38 +0200

    Configure Nagios to monitor all nodes
    All hosts are configured with their management IP with a ping check and
    have a basic SSH service checks for all IPs (except for public network).
    
    Note: each Nagios Hosts inherit from a Nagios Host Template to add Custom
    Variables (all IPs) per host.
    
    Change-Id: Ia65b0da45a8dd299406dc434a1dcdb8099c62b25

---
+++ deployment_scripts/puppet/modules/lma_infra_alerting/manifests/nagios/check_ssh.pp
@@ -0,0 +1,72 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# Configure SSH checks for per hostgroup
+#
+class lma_infra_alerting::nagios::check_ssh(
+  $management_hostgroups = {},
+  $private_hostgroups = {},
+  $storage_hostgroups = {},
+){
+
+  include lma_infra_alerting::params
+
+  $prefix = $lma_infra_alerting::params::nagios_config_filename_prefix
+
+  # create custom commands check_ssh with param using custom variables
+  $commands = {
+    check_ssh_storage => {
+      properties => {
+        command_line => "${nagios::params::nagios_plugin_dir}/check_ssh '\$_HOSTSTORAGE_ADDRESS\$'",
+      }
+    },
+    check_ssh_private => {
+      properties => {
+        command_line => "${nagios::params::nagios_plugin_dir}/check_ssh '\$_HOSTPRIVATE_ADDRESS\$'",
+      }
+    },
+  }
+  create_resources(nagios::command, $commands, {'prefix' => $prefix})
+
+  $service_checks = {
+    'SSH management network' => {
+      properties => {
+        hostgroup_name => $management_hostgroups,
+        check_command => $lma_infra_alerting::params::nagios_cmd_check_ssh,
+      }
+    },
+    'SSH storage network' => {
+      properties => {
+        hostgroup_name => $storage_hostgroups,
+        check_command => 'check_ssh_storage',
+      }
+    },
+  }
+
+  if ! empty($private_hostgroups){
+    $service_checks['SSH private'] = {
+        properties => {
+          hostgroup_name => $private_hostgroups,
+          check_command => 'check_ssh_private',
+        }
+      }
+  }
+  $default_services = {
+    prefix => $prefix,
+    defaults => {
+      'use' => $lma_infra_alerting::params::nagios_generic_service_template,
+    },
+  }
+  create_resources(nagios::service, $service_checks, $default_services)
+}
