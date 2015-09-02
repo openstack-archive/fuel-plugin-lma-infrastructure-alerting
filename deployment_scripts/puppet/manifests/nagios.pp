4b18dae82e5a99955fc5e45fa6971c85b490c63e
Swann Croiset <scroiset@mirantis.com>
2015-09-02 18:03:59 +0200

    [WIP] define new role lma_infrastructure_alerting
    Change-Id: I700c59c350942776b150d8f9c59bc0b92f3d037d

--- deployment_scripts/puppet/manifests/nagios.pp
+++ deployment_scripts/puppet/manifests/nagios.pp
@@ -13,6 +13,7 @@
 #    under the License.
 #
 
+notice('LMA infra alerting nagios')
 $management_vip = hiera('management_vip')
 $env_id = hiera('deployment_id')
 
@@ -43,139 +44,138 @@ if $storage_options['objects_ceph']{
   $services['openstack.swift.status'] = true
 }
 
-if $plugin['node_name'] == hiera('user_node_name') {
-  class { 'lma_infra_alerting':
-    openstack_deployment_name  => $env_id,
-    openstack_management_vip   => $management_vip,
-    additional_services        => keys($services),
-    # UI password
-    password                   => $password,
-  }
+class { 'lma_infra_alerting':
+  openstack_deployment_name  => $env_id,
+  openstack_management_vip   => $management_vip,
+  additional_services        => keys($services),
+  # UI password
+  password                   => $password,
+}
 
-  class { 'lma_infra_alerting::nagios::contact':
-    send_to         => $send_to,
-    send_from       => $send_from,
-    smtp_host       => $smtp_host,
-    smtp_auth       => $smtp_auth,
-    smtp_user       => $smtp_user,
-    smtp_password   => $smtp_password,
-    notify_warning  => $notify_warning,
-    notify_critical => $notify_critical,
-    notify_recovery => $notify_recovery,
-    notify_unknown  => $notify_unknown,
-    require         => Class['lma_infra_alerting'],
-  }
+class { 'lma_infra_alerting::nagios::contact':
+  send_to         => $send_to,
+  send_from       => $send_from,
+  smtp_host       => $smtp_host,
+  smtp_auth       => $smtp_auth,
+  smtp_user       => $smtp_user,
+  smtp_password   => $smtp_password,
+  notify_warning  => $notify_warning,
+  notify_critical => $notify_critical,
+  notify_recovery => $notify_recovery,
+  notify_unknown  => $notify_unknown,
+  require         => Class['lma_infra_alerting'],
+}
 
-  $nodes_hash = hiera('nodes', {})
-  $primary_controller_nodes = filter_nodes($nodes_hash,'role','primary-controller')
-  $controller_nodes = filter_nodes($nodes_hash,'role','controller')
-  $all_controller_nodes = concat($primary_controller_nodes, $controller_nodes)
+$nodes_hash = hiera('nodes', {})
+$primary_controller_nodes = filter_nodes($nodes_hash,'role','primary-controller')
+$controller_nodes = filter_nodes($nodes_hash,'role','controller')
+$all_controller_nodes = concat($primary_controller_nodes, $controller_nodes)
 
-  $compute_nodes = filter_nodes($nodes_hash,'role','compute')
-  $cinder_nodes = filter_nodes($nodes_hash,'role','cinder')
-  $base_os_nodes = filter_nodes($nodes_hash,'role','base-os')
-  $osd_nodes = filter_nodes($nodes_hash, 'role', 'ceph-osd')
+$compute_nodes = filter_nodes($nodes_hash,'role','compute')
+$cinder_nodes = filter_nodes($nodes_hash,'role','cinder')
+$base_os_nodes = filter_nodes($nodes_hash,'role','base-os')
+$osd_nodes = filter_nodes($nodes_hash, 'role', 'ceph-osd')
 
-  $all_nodes = {}
-  if !empty($all_controller_nodes){
-    $all_nodes['controller'] = $all_controller_nodes
-  }
+$all_nodes = {}
+if !empty($all_controller_nodes){
+  $all_nodes['controller'] = $all_controller_nodes
+}
 
-  if !empty($compute_nodes){
-    $all_nodes['compute'] = $compute_nodes
-  }
-  if !empty($cinder_nodes){
-    $all_nodes['cinder'] = $cinder_nodes
-  }
-  if !empty($base_os_nodes){
-    $all_nodes['base-os'] = $base_os_nodes
-  }
-  if !empty($osd_nodes){
-    $all_nodes['ceph-osd'] = $osd_nodes
-  }
+if !empty($compute_nodes){
+  $all_nodes['compute'] = $compute_nodes
+}
+if !empty($cinder_nodes){
+  $all_nodes['cinder'] = $cinder_nodes
+}
+if !empty($base_os_nodes){
+  $all_nodes['base-os'] = $base_os_nodes
+}
+if !empty($osd_nodes){
+  $all_nodes['ceph-osd'] = $osd_nodes
+}
 
-  class { 'lma_infra_alerting::nagios::hosts':
-    hosts                  => $all_nodes,
-    host_name_key          => 'name',
-    host_address_key       => 'internal_address',
-    host_display_name_keys => ['name', 'user_node_name'],
-    host_custom_vars_keys  => ['internal_address', 'private_address',
-                              'public_address', 'storage_address',
-                              'fqdn', 'role'],
-    require                => Class[lma_infra_alerting],
-  }
+class { 'lma_infra_alerting::nagios::hosts':
+  hosts                  => $all_nodes,
+  host_name_key          => 'name',
+  host_address_key       => 'internal_address',
+  host_display_name_keys => ['name', 'user_node_name'],
+  host_custom_vars_keys  => ['internal_address', 'private_address',
+                            'public_address', 'storage_address',
+                            'fqdn', 'role'],
+  require                => Class[lma_infra_alerting],
+}
 
 
-  # Nodes have private IPs only with GRE segmentation
-  $network_config = hiera('quantum_settings')
-  $segmentation_type = $network_config['L2']['segmentation_type']
-  if $segmentation_type == 'gre' {
-    $private_network = true
-  } else {
-    $private_network = false
-  }
+# Nodes have private IPs only with GRE segmentation
+$network_config = hiera('quantum_settings')
+$segmentation_type = $network_config['L2']['segmentation_type']
+if $segmentation_type == 'gre' {
+  $private_network = true
+} else {
+  $private_network = false
+}
 
-  # Configure SSH checks
-  lma_infra_alerting::nagios::check_ssh { 'management':
-    hostgroups => keys($all_nodes),
-    require    => Class[lma_infra_alerting],
-  }
+# Configure SSH checks
+lma_infra_alerting::nagios::check_ssh { 'management':
+  hostgroups => keys($all_nodes),
+  require    => Class[lma_infra_alerting],
+}
 
-  lma_infra_alerting::nagios::check_ssh { 'storage':
+lma_infra_alerting::nagios::check_ssh { 'storage':
+  hostgroups         => keys($all_nodes),
+  custom_var_address => 'storage_address',
+  require            => Class[lma_infra_alerting],
+}
+
+if $private_network {
+  lma_infra_alerting::nagios::check_ssh { 'private':
     hostgroups         => keys($all_nodes),
-    custom_var_address => 'storage_address',
+    custom_var_address => 'private_address',
     require            => Class[lma_infra_alerting],
   }
+}
 
-  if $private_network {
-    lma_infra_alerting::nagios::check_ssh { 'private':
-      hostgroups         => keys($all_nodes),
-      custom_var_address => 'private_address',
-      require            => Class[lma_infra_alerting],
-    }
+# Configure Grafana and InfluxDB checks
+$influxdb_grafana = hiera('influxdb_grafana', {})
+$influxdb_node_name = $influxdb_grafana['node_name']
+$influxdb_nodes = filter_nodes(hiera('nodes'), 'user_node_name', $influxdb_node_name)
+if ! empty($influxdb_nodes){
+  lma_infra_alerting::nagios::check_http { 'Grafana':
+     host_name                  => $influxdb_nodes[0]['name'],
+     port                       => $lma_infra_alerting::params::grafana_port,
+     url                        => '/login',
+     custom_var_address         => 'internal_address',
+     string_expected_in_content => 'grafana',
   }
-
-  # Configure Grafana and InfluxDB checks
-  $influxdb_grafana = hiera('influxdb_grafana', {})
-  $influxdb_node_name = $influxdb_grafana['node_name']
-  $influxdb_nodes = filter_nodes(hiera('nodes'), 'user_node_name', $influxdb_node_name)
-  if ! empty($influxdb_nodes){
-    lma_infra_alerting::nagios::check_http { 'Grafana':
-       host_name                  => $influxdb_nodes[0]['name'],
-       port                       => $lma_infra_alerting::params::grafana_port,
-       url                        => '/login',
-       custom_var_address         => 'internal_address',
-       string_expected_in_content => 'grafana',
-    }
-    lma_infra_alerting::nagios::check_http { 'InfluxDB':
-       host_name                  => $influxdb_nodes[0]['name'],
-       port                       => $lma_infra_alerting::params::influxdb_port,
-       url                        => '/ping',
-       custom_var_address         => 'internal_address',
-       string_expected_in_status  => '204 No Content',
-       string_expected_in_headers => 'X-Influxdb-Version',
-    }
-  }
-
-  # Configure Elasticsearch and Kibana checks
-  $es_kibana = hiera('elasticsearch_kibana', {})
-  $es_node_name = $es_kibana['node_name']
-  $es_kibana_nodes = filter_nodes(hiera('nodes'), 'user_node_name', $es_node_name)
-  if ! empty($es_kibana_nodes){
-    lma_infra_alerting::nagios::check_http { 'Kibana':
-       host_name                  => $es_kibana_nodes[0]['name'],
-       port                       => $lma_infra_alerting::params::kibana_port,
-       url                        => '/',
-       custom_var_address         => 'internal_address',
-       string_expected_in_content => 'Kibana 3',
-    }
-
-    lma_infra_alerting::nagios::check_http { 'Elasticsearch':
-       host_name                  => $es_kibana_nodes[0]['name'],
-       port                       => $lma_infra_alerting::params::elasticserach_port,
-       url                        => '/',
-       custom_var_address         => 'internal_address',
-       string_expected_in_content => '"status" : 200',
-    }
+  lma_infra_alerting::nagios::check_http { 'InfluxDB':
+     host_name                  => $influxdb_nodes[0]['name'],
+     port                       => $lma_infra_alerting::params::influxdb_port,
+     url                        => '/ping',
+     custom_var_address         => 'internal_address',
+     string_expected_in_status  => '204 No Content',
+     string_expected_in_headers => 'X-Influxdb-Version',
   }
 }
+
+# Configure Elasticsearch and Kibana checks
+$es_kibana = hiera('elasticsearch_kibana', {})
+$es_node_name = $es_kibana['node_name']
+$es_kibana_nodes = filter_nodes(hiera('nodes'), 'user_node_name', $es_node_name)
+if ! empty($es_kibana_nodes){
+  lma_infra_alerting::nagios::check_http { 'Kibana':
+     host_name                  => $es_kibana_nodes[0]['name'],
+     port                       => $lma_infra_alerting::params::kibana_port,
+     url                        => '/',
+     custom_var_address         => 'internal_address',
+     string_expected_in_content => 'Kibana 3',
+  }
+
+  lma_infra_alerting::nagios::check_http { 'Elasticsearch':
+     host_name                  => $es_kibana_nodes[0]['name'],
+     port                       => $lma_infra_alerting::params::elasticserach_port,
+     url                        => '/',
+     custom_var_address         => 'internal_address',
+     string_expected_in_content => '"status" : 200',
+  }
+}
+
