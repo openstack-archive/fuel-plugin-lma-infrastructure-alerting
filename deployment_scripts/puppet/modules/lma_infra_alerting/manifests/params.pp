4a3c9edd814ba400612ce4e5879c0828dab4ef95
Swann Croiset <scroiset@mirantis.com>
2016-02-10 10:36:29 +0100

    Remove localhost default monitoring
    This configuration is shipped with the package distribution but is
    annoying in the context of LMA.
    
    Fixes-bug: #1538655
    
    Change-Id: Iacc9c3ddfd0cc6b2445ea2df905a2efe73b2aace

--- deployment_scripts/puppet/modules/lma_infra_alerting/manifests/params.pp
+++ deployment_scripts/puppet/modules/lma_infra_alerting/manifests/params.pp
@@ -56,7 +56,11 @@ class lma_infra_alerting::params {
   case $::osfamily {
     'Debian': {
       $nagios_distribution_configs_to_purge = [
-        "${nagios::params::config_dir}/services_nagios2.cfg"]
+        "${nagios::params::config_dir}/services_nagios2.cfg",
+        "${nagios::params::config_dir}/localhost_nagios2.cfg",
+        "${nagios::params::config_dir}/hostgroups_nagios2.cfg",
+        "${nagios::params::config_dir}/extinfo_nagios2.cfg",
+        ]
     }
     default: {
       $nagios_distribution_configs_to_purge = []
