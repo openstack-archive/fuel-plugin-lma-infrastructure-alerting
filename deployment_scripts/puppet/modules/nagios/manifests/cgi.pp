5df73060291f02d8ad7dbfd4a9891b3817c2903c
Swann Croiset <scroiset@mirantis.com>
2016-06-15 15:04:50 +0200

    Use 2 Virtual IPs for Nagios
    One VIP is used for the Nagios UI and the other for the /status
    location exclusively used by LMA collectors to report statuses.
    This split will help to enable HTTPS for the Nagios UI and deploy it on
    another network while LMA collectors continue to use HTTP over the
    "monitoring" network.
    
    These VIPs are allocated by default on the management network.
    
    Change-Id: I3c0df769cb40b540b1afb74d4958e87b58daa12d

--- deployment_scripts/puppet/modules/nagios/manifests/cgi.pp
+++ deployment_scripts/puppet/modules/nagios/manifests/cgi.pp
@@ -17,11 +17,12 @@
 # Install and configure Nagios web interface
 #
 class nagios::cgi (
+  $vhost_listen_ip,
+  $wsgi_vhost_listen_ip = undef,
   $user = $nagios::params::cgi_user,
   $password = $nagios::params::cgi_password,
   $htpasswd_file = $nagios::params::cgi_htpasswd_file,
   $http_port = $nagios::params::cgi_http_port,
-  $vhost_listen_ip = '*',
   $wsgi_process_service_checks_location = '/status',
   $wsgi_process_service_checks_script = '/usr/local/bin/nagios-process-service-checks.wsgi',
   $wsgi_processes = 2,
@@ -45,21 +46,30 @@ class nagios::cgi (
     manage_user         => false,
   }
 
-  apache::listen { $http_port: }
-
-  # Template uses these variables: http_port, vhost_listen_ip, cgi_htpasswd_file
-  # nagios_command_file, wsgi_processes, wsgi_threads,
-  # wsgi_process_service_checks_script, wsgi_process_service_checks_location
-  $nagios_command_file = '/var/lib/nagios3/rw/nagios.cmd'
-  apache::custom_config { 'nagios':
-    content => template("nagios/${nagios::params::apache_vhost_config_tpl}"),
+  apache::listen { "${vhost_listen_ip}:${http_port}": }
+  if $wsgi_vhost_listen_ip {
+    apache::listen { "${wsgi_vhost_listen_ip}:${http_port}": }
   }
 
-  file { 'wsgi_process_service_checks_script':
-    ensure => present,
-    path   => $wsgi_process_service_checks_script,
-    source => 'puppet:///modules/nagios/process-service-checks.wsgi',
-    notify => Class['apache::service'],
+  # Template uses these variables: http_port, vhost_listen_ip, cgi_htpasswd_file
+  # nagios_command_file
+  $nagios_command_file = '/var/lib/nagios3/rw/nagios.cmd'
+  apache::custom_config { 'nagios-ui':
+    content => template("nagios/${nagios::params::apache_ui_vhost_config_tpl}"),
+  }
+  if $wsgi_vhost_listen_ip {
+    # Template uses these variables: http_port, cgi_htpasswd_file
+    # nagios_command_file, wsgi_vhost_listen_ip, wsgi_processes, wsgi_threads,
+    # wsgi_process_service_checks_script, wsgi_process_service_checks_location
+    apache::custom_config { 'nagios-wsgi':
+      content => template("nagios/${nagios::params::apache_wsgi_vhost_config_tpl}"),
+    }
+    file { 'wsgi_process_service_checks_script':
+      ensure => present,
+      path   => $wsgi_process_service_checks_script,
+      source => 'puppet:///modules/nagios/process-service-checks.wsgi',
+      notify => Class['apache::service'],
+    }
   }
 
   $apache_user = $apache::user
