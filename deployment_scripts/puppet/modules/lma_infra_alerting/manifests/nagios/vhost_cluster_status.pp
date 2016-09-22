58ebfac2a4c8cac6414d6b5c8db0491ed2a70424
Swann Croiset <scroiset@mirantis.com>
2016-09-22 13:39:09 +0200

    Fix Nagios service names for GSEs
    Change-Id: If62ee95acda21eb9979b65b071626a2ed257fd79

--- deployment_scripts/puppet/modules/lma_infra_alerting/manifests/nagios/vhost_cluster_status.pp
+++ deployment_scripts/puppet/modules/lma_infra_alerting/manifests/nagios/vhost_cluster_status.pp
@@ -51,6 +51,6 @@ define lma_infra_alerting::nagios::vhost_cluster_status(
     hostname              => $hostname,
     notifications_enabled => $notifications_enabled,
     contact_group         => $contact_group,
-    services              => prefix($services, "${title} "),
+    services              => prefix($services, "${title}-"),
   }
 }
